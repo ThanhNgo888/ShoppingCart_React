4d3366664ea9218215cfc1714d1394120b26be37
ThanhNgo <thanhngo26@hotmail.com>
2020-04-09 11:27:54 -0700

    conditional rendering

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -1,39 +1,26 @@
 //imrc => tab
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //cc => tab
 class Counter extends Component {
-    state = { 
-        count : 1,
-        tags: ["tag1", "tag2", "tag3"]
-    };
-    // =================================================================
-    render() { 
-        return (
-        <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag => 
-                <li key={tag}>{tag}</li>)}
-            </ul>
-        </div>
-        );
-    }
-    // =================================================================
-    getBadgeClasses(){
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-    // =================================================================
-    //using a function that returns a value
-    //if the property of this object equals zero, returns "Zero", otherwise returns the value of this object
-    formatCount() {
-        //use object destructing to pick the count property of this object(this.state) and storing it in a separate constant called "count"
-        const { count } = this.state;
-        return count === 0? <h6>Zero</h6> : count; 
-    }
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  // =================================================================
+  //helper method
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p> ;
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
+  // =================================================================
+  render() {
+    return (
+      <div>
+        {this.renderTags()}
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;

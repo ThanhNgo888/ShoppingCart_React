9d91a96f1817641986ae242d3df2cdcc83b602df
ThanhNgo <thanhngo26@hotmail.com>
2020-04-08 22:03:48 -0700

    embeded inline style to span

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -6,14 +6,10 @@ class Counter extends Component {
     state = { 
         count : 0
     };
-    styles={
-        fontSize:50,
-        fontWeight: "bold"
-    };
     render() { 
         return (
         <div>
-            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span style={{fontSize:30}} className="badge badge-primary m-2">{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );

5821f5a3039148fa9f1c5b8a25def6b837af4791
ThanhNgo <thanhngo26@hotmail.com>
2020-04-08 22:18:10 -0700

    using getBadgeClasses method to render classes

--- client/src/components/counter.jsx
+++ client/src/components/counter.jsx
@@ -4,19 +4,24 @@ import React, { Component } from 'react';
 //cc => tab
 class Counter extends Component {
     state = { 
-        count : 0
+        count : 1
     };
+    // =================================================================
     render() { 
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0? "badge-warning" : "badge-primary";
         return (
         <div>
-            <span className={classes}>{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
     }
-
+    // =================================================================
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    // =================================================================
     //using a function that returns a value
     //if the property of this object equals zero, returns "Zero", otherwise returns the value of this object
     formatCount() {
